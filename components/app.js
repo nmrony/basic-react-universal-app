33c5560e85bc6c4bcb936dd35fd71201c2fd9920
Nur Rony <pro.nmrony@gmail.com>
2015-12-13 20:41:09 +0600

    App bar added

--- components/app.js
+++ components/app.js
@@ -1,19 +1,23 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import  AppBar from 'material-ui/lib/app-bar';
+const  AppBar = require('material-ui/lib/app-bar');
+const FlatButton = require('material-ui/lib/flat-button');
 
 export default class AppComponent extends React.Component {
   render() {
+    const linkStyle = {
+      border: 0,
+      textDecoration: 'none',
+      color: '#fff'
+    };
+
     return (
       <div>
         <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more" />
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-        </ul>
+          showMenuIconButton = {false}
+          title={<Link to='/' style={linkStyle}>React Universal App</Link>}
+          iconElementRight={<FlatButton label="About" containerElement={<Link to="/about" />} linkButton={true} />} />
         { this.props.children }
       </div>
     );

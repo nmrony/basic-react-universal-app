732d75ec9c134daccf36e0c12974a121e7f6f77c
Nur Rony <pro.nmrony@gmail.com>
2015-12-13 13:38:14 +0600

    added app css and material ui

--- components/app.js
+++ components/app.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import  AppBar from 'material-ui/lib/app-bar';
 
 export default class AppComponent extends React.Component {
   render() {
     return (
       <div>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more" />
         <ul>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>

acd2264e05e097ebcb0b46e957ef8a5e3b33f9fa
Nur Rony <pro.nmrony@gmail.com>
2015-12-13 21:20:19 +0600

    added card design for index

--- components/index.js
+++ components/index.js
@@ -1,12 +1,29 @@
 
 import React from 'react';
+import Card from 'material-ui/lib/card/card';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardMedia from 'material-ui/lib/card/card-media';
+import CardText from 'material-ui/lib/card/card-text';
+import CardTitle from 'material-ui/lib/card/card-title';
 
 export default class IndexComponent extends React.Component {
   render() {
+    const cardStyle = {
+      margin: '10px  auto',
+      width: '60%'
+    };
+
     return (
-      <div>
-        <p>This is the index page</p>
-      </div>
+      <Card style={cardStyle}>
+        <CardTitle title="Welcome to App" subtitle="Come and embrace the awesomeness with React" />
+        <CardText>
+
+          <CardMedia>
+            <img src="/images/react.svg" height="300" width="300"/>
+          </CardMedia>
+
+        </CardText>
+      </Card>
     );
   }
 }
